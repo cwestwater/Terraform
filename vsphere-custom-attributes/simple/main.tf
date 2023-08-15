de9508a2795993e6010574705998982ce68904ef
Colin Westwater <cwestwater@gmail.com>
2023-08-15 19:56:15 +0100

    Add simple custom attribute example

---
+++ vsphere-custom-attributes/simple/main.tf
@@ -0,0 +1,28 @@
+terraform {
+
+  required_version = ">= 1.5.4"
+
+  required_providers {
+    vsphere = {
+      source  = "hashicorp/vsphere"
+      version = "2.4.1"
+    }
+  }
+}
+
+provider "vsphere" {
+  user                 = "administrator@vsphere.local"
+  password             = "VMware1!"
+  vsphere_server       = "vcsa-01.localdomain"
+  allow_unverified_ssl = true
+}
+
+resource "vsphere_custom_attribute" "built_by" {
+  name                = "Built By"
+  managed_object_type = "VirtualMachine"
+}
+
+resource "vsphere_custom_attribute" "repo" {
+  name                = "Repo"
+  managed_object_type = "VirtualMachine"
+}
