8f010caf57e5e7886bc50dec118300e47131c59b
Colin Westwater <cwestwater@gmail.com>
2023-10-23 19:45:02 +0100

    Update versions

--- vsphere-tags/simple/main.tf
+++ vsphere-tags/simple/main.tf
@@ -1,11 +1,11 @@
 terraform {
 
-  required_version = ">= 1.5.4"
+  required_version = ">= 1.6.2"
 
   required_providers {
     vsphere = {
       source  = "hashicorp/vsphere"
-      version = "2.4.1"
+      version = "2.5.1"
     }
   }
 }
